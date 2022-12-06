c5e6f51295e398e1a40a4d5a6787fdf4165b43ff
Eric Slosser <eric.slosser@nytimes.com>
2022-12-06 10:04:07 -0500

    day 6 pt2

--- day6.go
+++ day6.go
@@ -16,24 +16,25 @@ func main() {
 		os.Exit(1)
 	}
 
+	const uLength = 14
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		wholeLine := scanner.Text()
 		found := false
-		for i := 4; i < len(wholeLine) && !found; i++ {
-			c4 := wholeLine[i-4 : i]
+		for i := uLength; i < len(wholeLine) && !found; i++ {
+			c4 := wholeLine[i-uLength : i]
 			b := []byte(c4)
 			sort.Slice(b, func(i2, j int) bool {
 				return b[i2] < b[j]
 			})
 			// fmt.Println(c4, b)
-			// look for 4 unique
-			for j := 0; j < 13; j++ {
+			// look for uLength unique
+			for j := 0; j < uLength-1; j++ {
 				// fmt.Println(j, b[j], b[j+1], b[j] == b[j+1])
 				if b[j] == b[j+1] {
 					break
 				}
-				if j == 12 {
+				if j == uLength-2 {
 					found = true
 					fmt.Println("found", c4, "index", i)
 				}
