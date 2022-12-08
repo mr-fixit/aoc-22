5ff805bf99b4f0df8e8772423a29e1bbd9b79c24
Eric Slosser <eric.slosser@nytimes.com>
2022-12-08 14:58:59 -0500

    day7 1 clean

--- day8.go
+++ day8.go
@@ -8,7 +8,7 @@ import (
 
 func readInput() ([][]int, int, int) {
 	out := make([][]int, 0)
-	file, err := os.Open("day8_1.txt")
+	file, err := os.Open("day8_0.txt")
 	if err != nil {
 		fmt.Println("err: ", err)
 		os.Exit(1)
@@ -92,4 +92,42 @@ func main() {
 	// 582 too low
 	// 1642 too low
 	// part1: 1798
+
+	maxSceneScore := 0
+	sceneScore := 1
+	for x := 1; x < ncols-1; x++ {
+		for y := 1; y < nrows-1; y++ {
+
+			// for each tree in the interior
+			h := forest[y][x]
+			// fmt.Println("doing ", x, y, h)
+			for _, dxdy := range [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
+				fmt.Println("  dxdy:", dxdy)
+				dx := dxdy[0]
+				dy := dxdy[1]
+				directionScore := 0
+				for nx, ny := x+dx, y+dy; nx >= 0 && nx < ncols && ny >= 0 && ny < nrows; nx, ny = nx+dx, ny+dy {
+					directionScore += 1
+					// fmt.Println("     nx ny height[nx,ny]", nx, ny, forest[ny][nx])
+					if h <= forest[ny][nx] {
+						// fmt.Println("      hidden")
+						break // tree is hidden
+					}
+					if nx == 0 || nx == nrows-1 || ny == 0 || ny == ncols-1 {
+						// we made it to the edge, we must be visible
+						vis[y][x] = true
+						nVisible++
+						// fmt.Println("visible! now", nVisible)
+						break
+					}
+				}
+				sceneScore *= directionScore
+				fmt.Println(" dirScore", directionScore, sceneScore)
+			}
+		}
+	}
+	if sceneScore > maxSceneScore {
+		maxSceneScore = sceneScore
+	}
+	fmt.Println(" sceneScore, max", sceneScore, maxSceneScore)
 }
