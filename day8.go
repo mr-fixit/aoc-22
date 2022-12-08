9b92005f0a56f7e1c3a5169c3ef80e5429291930
Eric Slosser <eric.slosser@nytimes.com>
2022-12-08 12:31:34 -0500

    part 1, first working

--- day8.go
+++ day8.go
@@ -6,8 +6,8 @@ import (
 	"os"
 )
 
-func readInput() ([][]byte, int, int) {
-	out := make([][]byte, 0)
+func readInput() ([][]int, int, int) {
+	out := make([][]int, 0)
 	file, err := os.Open("day8_1.txt")
 	if err != nil {
 		fmt.Println("err: ", err)
@@ -15,7 +15,12 @@ func readInput() ([][]byte, int, int) {
 	}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		out = append(out, scanner.Bytes())
+		rowText := scanner.Text()
+		row := make([]int, len(rowText))
+		for i, c := range rowText {
+			row[i] = int(c - '0')
+		}
+		out = append(out, row)
 	}
 	return out, len(out), len(out[0])
 }
@@ -28,32 +33,70 @@ func makeVis(nrows, ncols int) (vis [][]bool) {
 	for x := 0; x < ncols; x++ {
 		for y := 0; y < nrows; y++ {
 			if x == 0 || x == ncols-1 || y == 0 || y == nrows-1 {
-				vis[x][y] = true
+				vis[y][x] = true
 			}
 		}
 	}
 	return
 }
 
+func print2D(m [][]int) {
+	for i, val := range m {
+		fmt.Printf("%d: %v\n", i, val)
+	}
+}
+
 func main() {
 	forest, nrows, ncols := readInput()
-	fmt.Println(nrows, ncols)
+	print2D(forest)
 
 	vis := makeVis(nrows, ncols)
 	nVisible := 2 * (nrows + ncols - 2)
 
-	for x := 1; x < ncols-2; x++ {
-		for y := 1; y < nrows-2; y++ {
-			h := forest[x][y]
-			if (vis[x-1][y] && forest[x-1][y] < h) ||
-				(vis[x+1][y] && forest[x+1][y] < h) ||
-				(vis[x][y-1] && forest[x][y-1] < h) ||
-				(vis[x][y+1] && forest[x][y+1] < h) {
-				vis[x][y] = true
-				nVisible++
+	for x := 1; x < ncols-1; x++ {
+		for y := 1; y < nrows-1; y++ {
+
+			// for each tree in the interior
+			h := forest[y][x]
+
+			for dx := -1; dx <= 1; dx += 2 {
+
+				for nx := x + dx; !vis[y][x] && nx >= 0 && nx < nrows; nx += dx {
+					if h <= forest[y][nx] {
+						break // tree is hidden
+					}
+					if nx == 0 || nx == nrows-1 {
+						// we made it to the edge, we must be visible
+						vis[y][x] = true
+						nVisible++
+						break
+					}
+				}
+			}
+			for dy := -1; dy <= 1; dy += 2 {
+				for ny := y + dy; !vis[y][x] && ny >= 0 && ny < ncols; ny += dy {
+					if h <= forest[ny][x] {
+						break
+					}
+					if ny == 0 || ny == ncols-1 {
+						// we made it to the edge, we must be visible
+						vis[y][x] = true
+						nVisible++
+						break
+					}
+				}
 			}
 		}
 	}
-	fmt.Println("part1: ", nVisible)
+	var status string
+	if nVisible == 1798 {
+		status = "RIGHT "
+	} else {
+		status = "WRONG"
+	}
+	fmt.Printf("part1: %d %s", nVisible, status)
 
+	// 582 too low
+	// 1642 too low
+	// part1: 1798
 }
