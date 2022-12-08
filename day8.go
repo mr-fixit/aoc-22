6ddf4026f206c9988f9beb50abcbe891214dbe75
Eric Slosser <eric.slosser@nytimes.com>
2022-12-08 15:22:49 -0500

    day7 2 clean

--- day8.go
+++ day8.go
@@ -8,7 +8,7 @@ import (
 
 func readInput() ([][]int, int, int) {
 	out := make([][]int, 0)
-	file, err := os.Open("day8_0.txt")
+	file, err := os.Open("day8_1.txt")
 	if err != nil {
 		fmt.Println("err: ", err)
 		os.Exit(1)
@@ -87,22 +87,22 @@ func main() {
 	} else {
 		status = "WRONG, should be 1798"
 	}
-	fmt.Printf("part1: %d %s", nVisible, status)
+	fmt.Printf("part1: %d %s\n\n", nVisible, status)
 
 	// 582 too low
 	// 1642 too low
 	// part1: 1798
 
 	maxSceneScore := 0
-	sceneScore := 1
 	for x := 1; x < ncols-1; x++ {
 		for y := 1; y < nrows-1; y++ {
 
+			sceneScore := 1
 			// for each tree in the interior
 			h := forest[y][x]
 			// fmt.Println("doing ", x, y, h)
 			for _, dxdy := range [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
-				fmt.Println("  dxdy:", dxdy)
+				// fmt.Println("  dxdy:", dxdy)
 				dx := dxdy[0]
 				dy := dxdy[1]
 				directionScore := 0
@@ -113,21 +113,21 @@ func main() {
 						// fmt.Println("      hidden")
 						break // tree is hidden
 					}
-					if nx == 0 || nx == nrows-1 || ny == 0 || ny == ncols-1 {
-						// we made it to the edge, we must be visible
-						vis[y][x] = true
-						nVisible++
-						// fmt.Println("visible! now", nVisible)
-						break
-					}
+					// if nx == 0 || nx == nrows-1 || ny == 0 || ny == ncols-1 {
+					// 	// we made it to the edge, we must be visible
+					// 	// fmt.Println("visible! now", nVisible)
+					// 	break
+					// }
 				}
 				sceneScore *= directionScore
-				fmt.Println(" dirScore", directionScore, sceneScore)
+				// fmt.Println(" dirScore", directionScore, sceneScore)
+			} // each direction
+			if sceneScore > maxSceneScore {
+				maxSceneScore = sceneScore
 			}
+			// fmt.Println("sceneScore, max", sceneScore, maxSceneScore)
 		}
 	}
-	if sceneScore > maxSceneScore {
-		maxSceneScore = sceneScore
-	}
-	fmt.Println(" sceneScore, max", sceneScore, maxSceneScore)
+	fmt.Println("part 2: ", maxSceneScore) // 259308 is right
+
 }
