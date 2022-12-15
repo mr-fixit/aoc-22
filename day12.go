6d401537cdd9dd97ad8b7c9e8e31ed9862e3036b
Eric Slosser <eric.slosser@nytimes.com>
2022-12-15 17:46:27 -0500

    day12 p1

--- day12.go
+++ day12.go
@@ -60,68 +60,77 @@ func findEnds(m ByteMap) (startX, startY, endX, endY int) {
 	return
 }
 
-func canGo(x, y, nx, ny int, b ByteMap, cost [][]int) (canGo bool) {
-	canGo = inBounds(nx, ny, b)
-	canGo = canGo && b.bytes[ny][nx]-b.bytes[y][x] <= 1
-	canGo = canGo && cost[ny][nx] > cost[y][x]+1
-	return
-}
-
 var sX, sY, eX, eY int
 
-func day12(fileName string) {
+func day12(fileName string, expectedValue int) {
 	var kMaxCost = 9999999
 	b := readInput_12(fileName)
 	sX, sY, eX, eY = findEnds(b)
 	fmt.Println("start: ", sX, sY)
 	fmt.Println("end: ", eX, eY)
-	redOnes := []Point{{sX, sY}, {eX, eY}}
-	drawMap12(b, redOnes)
+	drawMap12(b, Point{sX, sY}, Point{eX, eY})
 
-	unvisited := [][]int{}
+	unvisited := []Point{}
 	cost := make([][]int, b.nRows)
 	for y := 0; y < b.nRows; y++ {
 		cost[y] = make([]int, b.nCols)
 		for x := 0; x < b.nCols; x++ {
 			cost[y][x] = kMaxCost
-			unvisited = append(unvisited, []int{x, y})
+			unvisited = append(unvisited, Point{x, y})
 		}
 	}
 	cost[sY][sX] = 0
 
 	for len(unvisited) > 0 {
-		slices.SortFunc(unvisited, func(a, b []int) bool {
-			return cost[a[1]][a[0]] < cost[b[1]][b[0]]
+		slices.SortFunc(unvisited, func(a, b Point) bool {
+			return cost[a.y][a.x] < cost[b.y][b.x]
 		})
-		x := unvisited[0][0]
-		y := unvisited[0][1]
-		// fmt.Println("doing ", x, y, cost[y][x], abs(eX-x)+abs(eY-y))
+		x := unvisited[0].x
+		y := unvisited[0].y
+		fmt.Println("doing ", x, y, cost[y][x], abs(eX-x)+abs(eY-y))
 		unvisited = unvisited[1:]
 		curCost := cost[y][x]
-		//drawIntMap(cost, kMaxCost)
+		// drawIntMap(cost, kMaxCost)
+		// if x == 23 && y == 3 {
+		// 	println()
+		// }
 		for _, dxdy := range [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
 			newX := x + dxdy[0]
 			newY := y + dxdy[1]
-			if canGo(x, y, newX, newY, b, cost) {
+
+			canGo := inBounds(newX, newY, b)
+			canGo = canGo && slices.Contains(unvisited, Point{newX, newY})
+			canGo = canGo && int(b.bytes[newY][newX])-int(b.bytes[y][x]) <= 1
+			canGo = canGo && cost[newY][newX] > cost[y][x]+1
+
+			if canGo {
+				fmt.Println("		can go", newX, newY)
 				cost[newY][newX] = curCost + 1
 				if newX == eX && newY == eY {
 					fmt.Println("end cost", curCost+1)
-					os.Exit(0)
+					if curCost+1 != expectedValue {
+						fmt.Printf("error: got %d, wanted %d\n", cost[eY][eX], expectedValue)
+					}
+					break
 				}
 			}
 		}
 	}
 	drawIntMap(cost, kMaxCost)
+	fmt.Println("min cost", cost[eY][eX])
 }
 
-func drawMap12(m ByteMap, redOnes []Point) {
-	red := color.New(color.FgRed).PrintfFunc()
+func drawMap12(m ByteMap, start Point, end Point) {
+	printRed := color.New(color.FgRed).PrintfFunc()
 
 	for y := 0; y < m.nRows; y++ {
 		fmt.Printf("%3d: ", y)
 		for x := 0; x < m.nCols; x++ {
-			if slices.Contains(redOnes, Point{x, y}) {
-				red("%s", string(m.bytes[y][x]))
+			pt := Point{x, y}
+			if pt == start {
+				printRed("S")
+			} else if pt == end {
+				printRed("E")
 			} else {
 				fmt.Printf("%s", string(m.bytes[y][x]))
 			}
@@ -131,8 +140,13 @@ func drawMap12(m ByteMap, redOnes []Point) {
 }
 
 func drawIntMap(m [][]int, ignore int) {
+	fmt.Printf("   ")
+	for x := 0; x < len(m[0]); x++ {
+		fmt.Printf("%4d", x)
+	}
+	fmt.Println()
 	for y := 0; y < len(m); y++ {
-		fmt.Printf("%4d: ", y)
+		fmt.Printf("%3d", y)
 		for x := 0; x < len(m[y]); x++ {
 			v := m[y][x]
 			if v == ignore {
@@ -141,6 +155,7 @@ func drawIntMap(m [][]int, ignore int) {
 				fmt.Printf("%4d", v)
 			}
 		}
+		fmt.Println()
 		fmt.Println()
 	}
 }
